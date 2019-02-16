24fa2df04f04c1530c25604628c4f45b7375cf98
Yaroslav Veremenko <yaroslav@veremenko.info>
2019-02-15 19:11:58 -0700

    Converted to NROM

--- src/header.asm
+++ src/header.asm
@@ -4,7 +4,7 @@
     .byte $4e, $45, $53, $1a ; "NES" followed by MS-DOS EOF
     .byte $02                ; size of PRG ROM in 16 KiB units
     .byte $01                ; size of CHR ROM in 8 KiB units
-    .byte $30                ; horizontal mirroring, mapper 003 (CNROM)
+    .byte $00                ; horizontal mirroring, mapper 003 (CNROM)
     .byte $00                ; mapper 003 (CNROM)
 
     .byte $00                ; size of PRG RAM in 8 KiB units
